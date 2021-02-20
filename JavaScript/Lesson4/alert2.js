86c360ecef42b0cb8f65b54397c33ffdfac9b812
ma3atrak3r <ma3atrak3r@mail.ru>
2021-02-20 11:31:53 +0300

    ads

---
+++ JavaScript/Lesson4/alert2.js
@@ -0,0 +1,9 @@
+"use strict";
+const number = +prompt('Введите число');
+if (number > 0) {
+    alert("Число положительное");
+} else if (number < 0) {
+    alert("Отрицательное");
+} else {
+    alert("ноль")
+}
